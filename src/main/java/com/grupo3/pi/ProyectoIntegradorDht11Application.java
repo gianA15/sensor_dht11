b4faef066cbdce2339317356502dae56b53bf667
gianA15 <gian.arce@tecsup.edu.pe>
2022-11-13 22:22:31 -0500

    first commit

---
+++ src/main/java/com/grupo3/pi/ProyectoIntegradorDht11Application.java
@@ -0,0 +1,13 @@
+package com.grupo3.pi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntegradorDht11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIntegradorDht11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo3/pi/ProyectoIntegradorDht11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.pi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegradorDht11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
